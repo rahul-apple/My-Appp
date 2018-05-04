dc88c81b8997cef9032159447c55ef73e7e74767
Rahul Ramachandra <rahul-apple@users.noreply.github.com>
2018-05-04 13:20:54 +0530

    First Commit

---
+++ app/src/main/java/com/example/rahulr/myapplication/Defaults.java
@@ -0,0 +1,7 @@
+package com.example.rahulr.myapplication;
+
+public class Defaults {
+    public static final String APPLICATION_ID = "58099302-75F5-3C58-FF5A-EE07B25B1900";
+    public static final String API_KEY = "E9006F5A-21BD-1CD6-FFCE-369CB75E1100";
+    public static final String SERVER_URL = "http://localhost:8080/api";
+}
